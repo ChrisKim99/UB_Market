10611084e9eb19b5c82d3b21307bcc6ee56f5a6b
sooseokkim99@outlook.com <sooseokkim99@outlook.com>
2020-08-26 18:32:39 +0900

    first commit

---
+++ env/index/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # the first value will be the names of actions
+    path('', views.index, name="index"),
+    
+]
---
+++ env/index/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def index(request):
+# request has the value passed from clients
+# as specified in the seetings of main, the function will render pages from the templates
+# can return some value {"name": "value to return"}
+    return render(request, 'main/base.html')
