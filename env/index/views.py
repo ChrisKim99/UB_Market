e35f810d78769a2e1dd70fb46d85f0ff264bcfdf
sooseokkim99@outlook.com <sooseokkim99@outlook.com>
2020-08-27 06:49:08 +0900

    first commit

--- env/index/views.py
+++ env/index/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
+def base(request):
 # request has the value passed from clients
 # as specified in the seetings of main, the function will render pages from the templates
 # can return some value {"name": "value to return"}
