83e1996ea51f205c84b258036a4b023a83d2f92d
sooseokkim99@outlook.com <sooseokkim99@outlook.com>
2020-08-28 02:49:26 +0900

    base update

--- env/index/urls.py
+++ env/index/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     # the first value will be the names of actions
-    path('', views.base, name="base"),
+    path('', views.index, name="index"),
     
 ]
--- env/index/views.py
+++ env/index/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def base(request):
+def index(request):
 # request has the value passed from clients
 # as specified in the seetings of main, the function will render pages from the templates
 # can return some value {"name": "value to return"}
-    return render(request, 'main/base.html')
+    return render(request, 'main/index.html')
